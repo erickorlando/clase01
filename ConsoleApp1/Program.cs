a8d18045f3b092a4910267740dc57af00c5131b0
Erick Orlando <erickorlando@outlook.com>
2021-11-27 20:15:07 -0500

    Archivos de codigo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.Title = "Mi Primer Programa en C#";
+Console.WriteLine("Hello World!");
+var nombre = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(nombre))
+    Console.WriteLine($"Como te va {nombre}?");
+else
+    Console.WriteLine("Este es mi programa con .NET");
+
+int edad = 0;
+
+Console.WriteLine("Indica tu edad");
+
+edad = Convert.ToInt32(Console.ReadLine());
+
+var fecha = DateTime.Today.AddYears(edad * -1);
+
+Console.WriteLine($"Tu naciste en el año {fecha.Year}");
+Console.ReadLine();
